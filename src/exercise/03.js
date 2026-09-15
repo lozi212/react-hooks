50cafb5fe8e04644d391b5c56f8d35d02d2c735e
Loza Demeke <your-github-email@example.com>
2026-09-15 19:46:04 +0300

    complete exercise 03 useState greeting

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -1,21 +1,29 @@
+
 // Lifting state
 // http://localhost:3000/isolated/exercise/03.js
 
 import * as React from 'react'
 
-function Name({name, onNameChange}) {
+// Name now manages its own state (Extra Credit: colocating state)
+function Name() {
+  const [name, setName] = React.useState('')
+
   return (
     <div>
       <label htmlFor="name">Name: </label>
-      <input id="name" value={name} onChange={onNameChange} />
+      <input
+        id="name"
+        value={name}
+        onChange={event => setName(event.target.value)}
+      />
     </div>
   )
 }
 
-// 🐨 accept `animal` and `onAnimalChange` props to this component
+// FavoriteAnimal manages its own state
 function FavoriteAnimal() {
-  // 💣 delete this, it's now managed by the App
   const [animal, setAnimal] = React.useState('')
+
   return (
     <div>
       <label htmlFor="animal">Favorite Animal: </label>
@@ -28,28 +36,20 @@ function FavoriteAnimal() {
   )
 }
 
-// 🐨 uncomment this
-// function Display({name, animal}) {
-//   return <div>{`Hey ${name}, your favorite animal is: ${animal}!`}</div>
-// }
-
-// 💣 remove this component in favor of the new one
-function Display({name}) {
-  return <div>{`Hey ${name}, you are great!`}</div>
+// Extra Credit: Display only needs animal
+function Display({animal}) {
+  return <div>{`Your favorite animal is :!`}</div>
 }
 
 function App() {
-  // 🐨 add a useState for the animal
-  const [name, setName] = React.useState('')
   return (
     <form>
-      <Name name={name} onNameChange={event => setName(event.target.value)} />
-      {/* 🐨 pass the animal and onAnimalChange prop here (similar to the Name component above) */}
+      <Name />
       <FavoriteAnimal />
-      {/* 🐨 pass the animal prop here */}
-      <Display name={name} />
+      <Display />
     </form>
   )
 }
 
 export default App
+
